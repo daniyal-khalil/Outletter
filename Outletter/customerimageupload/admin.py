76b91d8fc46efdfe6db5ada5a14c8ced0987a3c1
usman <usman.kakakhel@ug.bilkent.edu.tr>
2021-01-25 15:58:49 +0300

    remove redundant app customer imageupload

--- Outletter/customerimageupload/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Outletter.customerimageupload.models import CustomerImageUpload
-
-# Register your models here.
-admin.site.register(CustomerImageUpload)
--- Outletter/customerimageupload/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomerimageuploadConfig(AppConfig):
-    name = 'CustomerImageUpload'
